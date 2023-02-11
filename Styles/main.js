542a71c26bbf954956292bb96b4e8b9655506cba
Triple-Zs <83244231+Triple-Zs@users.noreply.github.com>
2023-02-10 23:08:42 -0500

    Update main.js

--- Styles/main.js
+++ Styles/main.js
@@ -2,19 +2,19 @@ function ScriptCopy() {
   var Btn = document.getElementById("ScriptBtn");
   navigator.clipboard.writeText(`
 --[[
-    █████╗ ██╗███╗   ███╗██╗    ██╗ █████╗ ██████╗ ███████╗
-   ██╔══██╗██║████╗ ████║██║    ██║██╔══██╗██╔══██╗██╔════╝
-   ███████║██║██╔████╔██║██║ █╗ ██║███████║██████╔╝█████╗  
-   ██╔══██║██║██║╚██╔╝██║██║███╗██║██╔══██║██╔══██╗██╔══╝  
-   ██║  ██║██║██║ ╚═╝ ██║╚███╔███╔╝██║  ██║██║  ██║███████╗
-   ╚═╝  ╚═╝╚═╝╚═╝     ╚═╝ ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝
-   
-   Supported Games
-     [+ MAIN] DaHood, Untitled Hood, Hood Modded, Hood Customs
-     [+ OTHERS] DaHood Rev Trainer, + more
+    ██████╗ ██╗     ██╗   ██╗████████╗ ██████╗ 
+    ██╔══██╗██║     ██║   ██║╚══██╔══╝██╔═══██╗
+    ██████╔╝██║     ██║   ██║   ██║   ██║   ██║
+    ██╔═══╝ ██║     ██║   ██║   ██║   ██║   ██║
+    ██║     ███████╗╚██████╔╝   ██║   ╚██████╔╝
+    ╚═╝     ╚══════╝ ╚═════╝    ╚═╝    ╚═════╝ 
+                                           
+    Supported Games
+      [+ MAIN] DaHood, Untitled Hood, Hood Modded, Hood Customs
+      [+ OTHERS] DaHood Rev Trainer, + more
 ]]--
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Triple-Zs/u/main/aimware"))()
+loadstring(game:HttpGet("https://encrypts.lol/u/source.lua"))()
   `);
   Btn.innerText = "Copied"
   setTimeout(() => { Btn.innerText = "Copy Script" }, 500)
